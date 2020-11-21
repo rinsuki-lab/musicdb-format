d0f956e4c3e5715b81375c81d315716e3efb8234
rinsuki <428rinsuki+git@gmail.com>
2020-11-21 11:06:23 +0900

    update

--- HexFiend_BinaryTemplates/MusicDB_Header.tcl
+++ HexFiend_BinaryTemplates/MusicDB_Header.tcl
@@ -7,7 +7,7 @@
 
 little_endian
 requires 0 "68 66 6D 61" ;# hfma
-move 4
+ascii 4 "FourCC (=hfma)"
 uint32 "Header size"
 uint32 "File size"
 uint32 "unknown (file flag? & 1=always,& 0x10000=opened in bigsur)" ;# 1=always?, 0x10000=bigsur?
@@ -21,9 +21,20 @@ uint32 "Playlists" ;# minus 5 + device count?
 uint32 "Albums"
 uint32 "Artists"
 uint32 "Max Crypt Size? (always 102400?)"
-uint32 "unknown (always 32400?)"
+int32 "UTC Offset (seconds)"
 uint32 "Linked Apple ID"
 uint32 "unknown (always 2?)"
 macdate "Library Updated At"
 uint32 "Library type? (0=notlib,1=music,2=tv)" ;# 0 = file type is not library, 1 = Music, 2 = TV
 hex 8 "Library Persistent ID (from migrated iTunes Library?)"
+uint32 "unknown (always 722?)"
+uint32 "unknown"
+uint32 "unknown"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
+uint32 "unknown (always 0?)"
